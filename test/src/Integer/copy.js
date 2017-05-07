a99a028e74155e52249623401c67be2c7ce8110d
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-08 01:33:50 +0200

    test copy

---
+++ test/src/Integer/copy.js
@@ -0,0 +1,30 @@
+import test from 'ava' ;
+import { ZZ } from '../../../src' ;
+
+test ( 'Integer#copy' , t => {
+
+	const s = '34998403804830' ;
+
+	const a = ZZ.from(s) ;
+	const b = a.copy();
+
+	t.is(a.toString(), s);
+	t.is(b.toString(), s);
+
+	a.negate();
+
+	t.is(a.toString(), '-'+s);
+	t.is(b.toString(), s);
+
+	a.isub(a);
+
+	t.is(a.toString(), '0');
+	t.is(b.toString(), s);
+
+	a.iadd(b);
+
+	t.is(a.toString(), s);
+	t.is(b.toString(), s);
+
+
+} ) ;
