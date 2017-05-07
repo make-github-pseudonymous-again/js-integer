f5b28ee0926c4ed835d4804f1a90bd0081828a98
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-08 00:58:17 +0200

    test Integer#ne

--- test/src/ZZ/$1.js
+++ test/src/ZZ/$1.js
@@ -10,21 +10,25 @@ test( '$1.le(2)' , t => { t.true ( ZZ.$1().le( ZZ.from(2) ) ) ; } ) ;
 test( '$1.eq(2)' , t => { t.false( ZZ.$1().eq( ZZ.from(2) ) ) ; } ) ;
 test( '$1.ge(2)' , t => { t.false( ZZ.$1().ge( ZZ.from(2) ) ) ; } ) ;
 test( '$1.gt(2)' , t => { t.false( ZZ.$1().gt( ZZ.from(2) ) ) ; } ) ;
+test( '$1.ne(2)' , t => { t.true ( ZZ.$1().ne( ZZ.from(2) ) ) ; } ) ;
 
 test( '$1.lt(1)' , t => { t.false( ZZ.$1().lt( ZZ.from(1) ) ) ; } ) ;
 test( '$1.le(1)' , t => { t.true ( ZZ.$1().le( ZZ.from(1) ) ) ; } ) ;
 test( '$1.eq(1)' , t => { t.true ( ZZ.$1().eq( ZZ.from(1) ) ) ; } ) ;
 test( '$1.ge(1)' , t => { t.true ( ZZ.$1().ge( ZZ.from(1) ) ) ; } ) ;
 test( '$1.gt(1)' , t => { t.false( ZZ.$1().gt( ZZ.from(1) ) ) ; } ) ;
+test( '$1.ne(1)' , t => { t.false( ZZ.$1().ne( ZZ.from(1) ) ) ; } ) ;
 
 test( '$1.lt(0)' , t => { t.false( ZZ.$1().lt( ZZ.from(0) ) ) ; } ) ;
 test( '$1.le(0)' , t => { t.false( ZZ.$1().le( ZZ.from(0) ) ) ; } ) ;
-test( '$1.eq(0)' , t => { t.false ( ZZ.$1().eq( ZZ.from(0) ) ) ; } ) ;
+test( '$1.eq(0)' , t => { t.false( ZZ.$1().eq( ZZ.from(0) ) ) ; } ) ;
 test( '$1.ge(0)' , t => { t.true ( ZZ.$1().ge( ZZ.from(0) ) ) ; } ) ;
 test( '$1.gt(0)' , t => { t.true ( ZZ.$1().gt( ZZ.from(0) ) ) ; } ) ;
+test( '$1.ne(0)' , t => { t.true ( ZZ.$1().ne( ZZ.from(0) ) ) ; } ) ;
 
 test( '$1.lt(-1)' , t => { t.false( ZZ.$1().lt( ZZ.from(-1) ) ) ; } ) ;
 test( '$1.le(-1)' , t => { t.false( ZZ.$1().le( ZZ.from(-1) ) ) ; } ) ;
 test( '$1.eq(-1)' , t => { t.false( ZZ.$1().eq( ZZ.from(-1) ) ) ; } ) ;
 test( '$1.ge(-1)' , t => { t.true ( ZZ.$1().ge( ZZ.from(-1) ) ) ; } ) ;
 test( '$1.gt(-1)' , t => { t.true ( ZZ.$1().gt( ZZ.from(-1) ) ) ; } ) ;
+test( '$1.ne(-1)' , t => { t.true ( ZZ.$1().ne( ZZ.from(-1) ) ) ; } ) ;
