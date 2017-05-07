326f6642d8e74d309dec02c4d59ce0bc40bc70e5
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-08 01:53:55 +0200

    cover $0 , $1

---
+++ test/src/integer/constants.js
@@ -0,0 +1,6 @@
+import test from 'ava' ;
+
+import { ZZ , $0 , $1 } from '../../../src' ;
+
+test( '$0 == ZZ.$0' , t => { t.true($0().eq(ZZ.$0())) ; } ) ;
+test( '$1 == ZZ.$1' , t => { t.true($1().eq(ZZ.$1())) ; } ) ;
