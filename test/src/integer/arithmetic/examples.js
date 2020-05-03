985d79c615f2df6ba901900b86e802e4b012ff7c
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-03 23:25:25 +0200

    :mag: test: Add test for example code.

---
+++ test/src/integer/arithmetic/examples.js
@@ -0,0 +1,15 @@
+import test from 'ava' ;
+import { parse , stringify , add } from '../../../../src' ;
+
+function macro ( t , A , x , B , y , C , z ) {
+    const a = parse( A , x ) ;
+    const b = parse( B , y ) ;
+    const c = add( a , b ) ;
+    t.is( stringify( a , x ) , A ) ;
+    t.is( stringify( b , y ) , B ) ;
+    t.is( stringify( c , z ) , C ) ;
+}
+
+macro.title = ( _ , A , x , B , y , C , z ) => `${A}_${x} + ${B}_${y} = ${C}_${z}` ;
+
+test( macro , 'dead' , 16 , '101010' , 2 , '57047' , 10 ) ;
