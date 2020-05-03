71d506d61ba8c3de9d680534849cb6bba917cd93
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-03 23:51:27 +0200

    :mag: test(ZZ.from): Add one more test 0x3b.

--- test/src/ZZ/from.js
+++ test/src/ZZ/from.js
@@ -8,6 +8,7 @@ test( 'ZZ.from()' , t => {
 	t.is( ZZ.from(undefined).toString() , '0' ) ;
 
 	t.is( ZZ.from('zz', 36).toString(36) , 'zz' ) ;
+	t.is( ZZ.from('3b', 16).toString(16) , '3b' ) ;
 	//t.is( ZZ.from('0x3b').toString(16) , '3b' ) ;
 	t.is( ZZ.from(ZZ.from(5).digits(3), 3).toString() , '5' ) ;
 	t.is( ZZ.from(Math.pow(7, 7)).toString() , '823543' ) ;
