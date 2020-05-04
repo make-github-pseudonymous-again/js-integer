77a5d260843db5d681349d6c6730d54c80948fbc
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-04 11:34:59 +0200

    :mag: test: Generate more tests and cover `n` instructions.

--- test/generate.py
+++ test/generate.py
@@ -1,28 +1,29 @@
 import operator
 
-hugenumbers = [
-    1 ,
-    17 ,
+MAX_NUMBER = 2**53 - 1
+MIN_NUMBER = -2**53
+
+hugenumbers = sorted([
     91**7 ,
     2**30 ,
     3**50
-]
+])
 
-smallnumbers = [ 1 , 3 , 7 , 9 , 11 , 17 , 22 , 24 , 27 , 29 ]
+smallnumbers = sorted([ 1 , 3 , 7 , 9 , 11 , 17 , 22 , 24 , 27 , 29 , 1234 , 5678 ])
 
 arithmetic = {
     'add' : {
-        'numbers' : hugenumbers ,
+        'numbers' : smallnumbers + hugenumbers ,
         'apply' : operator.add ,
         'str' : '+'
     } ,
     'sub' : {
-        'numbers' : hugenumbers ,
+        'numbers' : smallnumbers + hugenumbers ,
         'apply' : operator.sub ,
         'str' : '-'
     } ,
     'mul' : {
-        'numbers' : hugenumbers ,
+        'numbers' : smallnumbers + hugenumbers ,
         'apply' : operator.mul ,
         'str' : '*'
     } ,
@@ -32,30 +33,30 @@ arithmetic = {
         'str' : '^'
     } ,
     'div' : {
-        'numbers' : hugenumbers ,
+        'numbers' : smallnumbers + hugenumbers ,
         'apply' : operator.floordiv ,
         'str' : '/'
     } ,
     'mod' : {
-        'numbers' : hugenumbers ,
+        'numbers' : smallnumbers + hugenumbers ,
         'apply' : operator.mod ,
         'str' : '%'
     }
 }
 
-def write ( f , numbers , name , t , opstr , a , ispow = True ) :
+def write ( f , numbers , name , t , ispow = False , isn = False , isi = False) :
 
     f.write("import test from 'ava' ;\n")
     f.write("import {{ parse , stringify , {} }} from '../../../../src' ;\n\n".format(name))
 
-    if ispow :
+    if isn :
 
         f.write("""function macro ( t , A , B , C ) {{
     const a = parse( A ) ;
     const c = {}( a , B ) ;
     t.is( stringify( a ) , {} ) ;
     t.is( stringify( c ) , C ) ;
-}}\n\n""".format( name , a ) )
+}}\n\n""".format( name , 'C' if isi else 'A'  ) )
 
     else:
 
@@ -66,67 +67,62 @@ def write ( f , numbers , name , t , opstr , a , ispow = True ) :
     t.is( stringify( a ) , {} ) ;
     t.is( stringify( b ) , B ) ;
     t.is( stringify( c ) , C ) ;
-}}\n\n""".format( name , a ) )
+}}\n\n""".format( name , 'C' if isi else 'A' ) )
 
 
-    f.write("macro.title = ( _ , A , B , C ) => `${{A}} {} ${{B}} = ${{C}}` ;\n\n".format(opstr))
+    f.write("macro.title = ( _ , A , B , C ) => `{}(${{A}},${{B}}) = ${{C}}` ;\n\n".format(name))
+
+    if isn:
+        LINE = "test( macro , '{}' , {} , '{}' ) ;\n"
+    else:
+        LINE = "test( macro , '{}' , '{}' , '{}' ) ;\n"
 
     for a in numbers :
 
         for b in numbers :
 
-            if ispow:
+            x = a
+            y = b
+            c = t( x , y )
+            if not isn or MIN_NUMBER <= y <= MAX_NUMBER:
+                f.write(LINE.format(x,y,c))
 
-                x = a
-                y = b
-                c = t( x , y )
-                f.write("test( macro , '{}' , {} , '{}' ) ;\n".format(x,y,c))
+            x = -a
+            y = b
+            c = t( x , y )
+            if not isn or MIN_NUMBER <= y <= MAX_NUMBER:
+                f.write(LINE.format(x,y,c))
 
-                x = -a
-                y = b
-                c = t( x , y )
-                f.write("test( macro , '{}' , {} , '{}' ) ;\n".format(x,y,c))
-
-            else:
-
-                x = a
-                y = b
-                c = t( x , y )
-                f.write("test( macro , '{}' , '{}' , '{}' ) ;\n".format(x,y,c))
-
-                x = -a
-                y = b
-                c = t( x , y )
-                f.write("test( macro , '{}' , '{}' , '{}' ) ;\n".format(x,y,c))
+            if not ispow:
 
                 x = a
                 y = -b
                 c = t( x , y )
-                f.write("test( macro , '{}' , '{}' , '{}' ) ;\n".format(x,y,c))
+                if not isn or MIN_NUMBER <= y <= MAX_NUMBER:
+                    f.write(LINE.format(x,y,c))
 
                 x = -a
                 y = -b
                 c = t( x , y )
-                f.write("test( macro , '{}' , '{}' , '{}' ) ;\n".format(x,y,c))
+                if not isn or MIN_NUMBER <= y <= MAX_NUMBER:
+                    f.write(LINE.format(x,y,c))
+
+def open_and_write ( opname , t , nb , **kwargs ) :
+    with open( 'test/src/integer/arithmetic/{}.js'.format(opname) , 'w' ) as f :
+        write( f , nb , opname , t , **kwargs )
 
 for name , op in arithmetic.items():
 
     t = op['apply']
     nb = op['numbers']
 
+    ispow = name == 'pow'
+
     # standard op
-    with open( 'test/src/integer/arithmetic/{}.js'.format(name) , 'w' ) as f :
-
-        opstr = op['str']
-        a = 'A'
-
-        write( f , nb , name , t , opstr , a , ispow = name == 'pow' )
-
+    open_and_write( name , t , nb , ispow = ispow )
     # in-place op
-    iname = 'i{}'.format(name)
-    with open( 'test/src/integer/arithmetic/{}.js'.format(iname) , 'w' ) as f :
-
-        opstr = op['str'] + '='
-        a = 'C'
-
-        write( f , nb , iname , t , opstr , a , ispow = name == 'pow' )
+    open_and_write( 'i' + name , t , nb , isi = True , ispow = ispow )
+    # standard op with number arg
+    open_and_write( name + 'n' , t , nb , isn = True , ispow = ispow )
+    # in-place op with number arg
+    open_and_write( 'i' + name + 'n' , t , nb , isi = True , isn = True , ispow = ispow )
