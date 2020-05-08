73c9492546d5bfee22d644bc07c349495f9c8ac1
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-08 11:36:55 +0200

    :rocket: perf: Max out representation base to save cycles and memory.

--- src/DEFAULT_REPRESENTATION_BASE.js
+++ src/DEFAULT_REPRESENTATION_BASE.js
@@ -1,4 +1,5 @@
-import { DEFAULT_DISPLAY_BASE } from './' ;
+//import { DEFAULT_DISPLAY_BASE } from './' ;
+import { MAX_BASE } from './_limits' ;
 
 /*
  * Biggest power of DEFAULT_DISPLAY_BASE whose square holds in a single double
@@ -6,4 +7,7 @@ import { DEFAULT_DISPLAY_BASE } from './' ;
  * and for fast base DEFAULT_DISPLAY_BASE printing. On older machines with no
  * native doubles, it might be worth to only use ints.
  */
-export const DEFAULT_REPRESENTATION_BASE = Math.pow(DEFAULT_DISPLAY_BASE,Math.floor(Math.log(Math.sqrt(Math.pow(2,53)))/Math.log(DEFAULT_DISPLAY_BASE))) ;
+//export const DEFAULT_REPRESENTATION_BASE = Math.pow(DEFAULT_DISPLAY_BASE,Math.floor(Math.log(Math.sqrt(Math.pow(2,53)))/Math.log(DEFAULT_DISPLAY_BASE))) ;
+//export const DEFAULT_REPRESENTATION_BASE = Math.pow(16,Math.floor(Math.log(Math.sqrt(Math.pow(2,53)))/Math.log(16))) ;
+//export const DEFAULT_REPRESENTATION_BASE = Math.pow(2,26) | 0;
+export const DEFAULT_REPRESENTATION_BASE = MAX_BASE;
